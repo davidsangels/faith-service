215d60ea7da5f7b0032a1a12460e13fb25d2af38
Faith <pfaithmtan@gmail.com>
2019-07-06 00:04:29 -0700

    Change paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 var path = require('path');
-// var SRC_DIR = path.join(__dirname, '/client/src');
 // var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: path.join(__dirname, '/client/index.js'),
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: path.resolve(__dirname, 'public')
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
-        include : SRC_DIR,
         loader : 'babel-loader',      
         query: {
           presets: ['react', 'es2015']
         }
       }
-    ]
-  }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+}
 };
