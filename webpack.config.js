732a10589694af953896d14d4d03f12dd67235ad
Faith <pfaithmtan@gmail.com>
2019-07-06 00:22:34 -0700

    Fix presets

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
         test : /\.jsx?/,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-        }
+        use: {
+            loader : 'babel-loader', 
+            query: {
+              presets: ['@babel/react', '@babel/env']
+            }
+        }     
       }
     ],
   },
