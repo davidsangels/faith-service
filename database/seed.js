7f8f48f785a0885cedcb7d230eef0389f3f1af5b
Faith <pfaithmtan@gmail.com>
2019-07-06 13:04:36 -0700

    Model user and place

--- database/seed.js
+++ database/seed.js
@@ -25,8 +25,73 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+const Model = Sequelize.Model;
+class User extends Model {}
+User.init({
+  first_name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  last_name: {
+    type: Sequelize.STRING
+  }
+}, {
+  sequelize,
+  modelName: 'user'
+});
 
+class Place extends Model {}
+Place.init({
+  place_image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  place_location: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  accommodation_type: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  place_name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'place'
+});
 
+// CREATE TABLE IF NOT EXISTS places (
+//   id INT AUTO_INCREMENT,
+//   place_image TEXT NOT NULL,
+//   place_location VARCHAR(255) NOT NULL,
+//   accommodation_type VARCHAR(255) NOT NULL,
+//   place_name VARCHAR(255) NOT NULL,
+//   price INT NOT NULL,
+//   rating FLOAT,
+//   PRIMARY KEY (id)
+// );
+
+// CREATE TABLE IF NOT EXISTS favourites (
+//   id INT AUTO_INCREMENT,
+//   listing_info TEXT NOT NULL,
+//   space_info TEXT NOT NULL,
+//   reviews TEXT NOT NULL,
+//   review_date VARCHAR(255) NOT NULL,
+//   place_id INT UNSIGNED NOT NULL REFERENCES places(id),
+//   id_users INT UNSIGNED NOT NULL REFERENCES users(id),
+//   PRIMARY KEY (id)
+// );
 
   // const placeType = ['ENTIRE HOUSE', 'ENTIRE VILLA', 'ENTIRE APARTMENT'];
 
