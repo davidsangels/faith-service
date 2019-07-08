e40491cfd0dceb895720f5f679c4da399b1b7f0e
Faith <pfaithmtan@gmail.com>
2019-07-07 17:58:21 -0700

    Handle connection error

--- database/index.js
+++ database/index.js
@@ -5,7 +5,14 @@ var connection = mysql.createConnection({
   password : '',
   database : 'related_listings'
 });
- 
-connection.connect();
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 module.exports = connection;
