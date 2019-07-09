9c7e8efb90c4d03f913aa81378ddf75f3cdcf8c5
Faith <pfaithmtan@gmail.com>
2019-07-08 22:42:21 -0700

    Correct string concatenation by interpolation

--- database/index.js
+++ database/index.js
@@ -1,18 +1,19 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'related_listings'
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'related_listings'
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
 
-  console.log('connected as id ' + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
