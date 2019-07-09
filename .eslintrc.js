f89b206b76709f453c2981c78c639cb5e9397d48
Faith <pfaithmtan@gmail.com>
2019-07-08 21:21:27 -0700

    Change to use airbnb guide and add rules
    - Switch from Hack Reactor style guide to Airbnb style guide
    - Add custom rule to ignore trailing comma and console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@
  */
 
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  extends: 'airbnb',
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-console': 'off'
+  }
 };
