15fdeacac44e29f67a101e0d09ee7a8b81666cfe
Faith <pfaithmtan@gmail.com>
2019-07-10 04:51:25 -0700

    Correct syntax
    - Add class name to div
    - Destructure props
    - Declare unique key for Places component

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Places from './Places';
 
-const Carousel = props => (
-  <div className="parent">
-    { (props.data).map(place => <Places place={place} />) }
-  </div>
-);
+const Carousel = (props) => {
+  const { data } = props;
+  return (
+    <div className="parent">
+      {data.map(place => <Places key={place.id} place={place} />) }
+    </div>
+  );
+};
+
+// (
+  // <div className="parent">
+  //   {data.map(place => <Places place={place} />) }
+  // </div>
+// );
 
 export default Carousel;
