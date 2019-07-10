10bc6dd3dd9ca12e40dfb274740fd6b9a088412f
Faith <pfaithmtan@gmail.com>
2019-07-10 04:58:22 -0700

    Correct props
    - Adjust property names due to change of props being passed down
    - Change data from hard-code to data from database

--- client/components/Places.jsx
+++ client/components/Places.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 const Places = props => (
   <div className="places">
-      <img src={props.place.photo} />
-    <div>{props.place.location}</div>
-    <div>{props.place.name}</div>
+    <img src={props.place.place_image} />
+    <div>{props.place.place_location}</div>
+    <div>{props.place.place_name}</div>
     <div>{props.place.stars}</div>
-    <div>{props.place.reviewNum}</div>
+    <div>{props.place.rating}</div>
   </div>
 );
+
+// const Places = props => (
+//   <div className="places">
+//     {console.log(props)}
+//     <img src={props.place.photo} />
+//     <div>{props.place.location}</div>
+//     <div>{props.place.name}</div>
+//     <div>{props.place.stars}</div>
+//     <div>{props.place.reviewNum}</div>
+//   </div>
+// );
+
 
 export default Places;
