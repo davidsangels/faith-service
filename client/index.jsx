9da19ba1c573d89ea591f19cad75c49e6893ff82
Faith <pfaithmtan@gmail.com>
2019-07-10 00:54:15 -0700

    Add hard-coded data

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const data = [
+  {
+    photo: 'https://loremflickr.com/320/240/dog',
+    name: 'Faith',
+    location: 'Shibuya District',
+    stars: 5,
+    reviewNum: 120
+  },
+  {
+    photo: 'https://loremflickr.com/320/240/dog',
+    name: 'ABC',
+    location: 'Tehama street',
+    stars: 2,
+    reviewNum: 220
+  },
+  {
+    photo: 'https://loremflickr.com/320/240/dog',
+    name: 'Dorrisssss',
+    location: 'I dunno, somewhere sitting',
+    stars: 5,
+    reviewNum: 222
+  }
+];
+
+ReactDOM.render(<App data={data} />, document.getElementById('app'));
