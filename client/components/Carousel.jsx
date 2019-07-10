089d0ad7ad4236f7586ed34a60fe4ecaccff5328
Faith <pfaithmtan@gmail.com>
2019-07-10 00:46:10 -0700

    Create component with hard-coded data
    - Map hard-coded data to Places
    - Add class name to div

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Places from './Places';
 
-const Carousel = () => (
-  <div>
-    Hello
+const Carousel = props => (
+  <div className="parent">
+    { (props.data).map(place => <Places place={place} />) }
   </div>
 );
 
