e0f586a3a919ec7a84376925e39cbd99add3dc06
Faith <pfaithmtan@gmail.com>
2019-07-24 21:15:02 -0700

    Remove hard coded data

--- client/index.jsx
+++ client/index.jsx
@@ -2,28 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const data = [
-  {
-    photo: 'https://loremflickr.com/320/240/dog',
-    name: 'Faith',
-    location: 'Shibuya District',
-    stars: 5,
-    reviewNum: 120
-  },
-  {
-    photo: 'https://loremflickr.com/320/240/dog',
-    name: 'ABC',
-    location: 'Tehama street',
-    stars: 2,
-    reviewNum: 220
-  },
-  {
-    photo: 'https://loremflickr.com/320/240/dog',
-    name: 'Dorrisssss',
-    location: 'I dunno, somewhere sitting',
-    stars: 5,
-    reviewNum: 222
-  }
-];
-
-ReactDOM.render(<App data={data} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
